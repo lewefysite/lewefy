922eb2d7f23f30c81bf82124299ab5416aaa4dc7
lewefysite <felipecaxeiro@gmail.com>
2025-08-25 20:54:11 -0300

    Update auth.controller.ts

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,13 +1,11 @@
 import { Controller, Post, Body, Get, Query, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { RegisterUserDto } from './dto/register-user.dto';
-import { RecaptchaGuard } from '../common/guards/recaptcha.guard'; // Ajustei para caminho relativo
+import { RecaptchaGuard } from '../common/guards/recaptcha.guard';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
-
-  // ... outros endpoints que você possa ter (ex: login)
 
   @Get('confirm-email')
   async confirmEmail(@Query('token') token: string) {
