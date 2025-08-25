b6b0f41fee3811c62e68dc72c7fc12379671b2b7
lewefysite <felipecaxeiro@gmail.com>
2025-08-25 20:53:40 -0300

    Update app.module.ts

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
-import { EmailModule } from './mail/email.module'; // Caminho corrigido
+import { EmailModule } from './mail/email.module';
 
 @Module({
   imports: [
