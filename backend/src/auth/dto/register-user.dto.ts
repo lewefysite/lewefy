1fce2f92cb9e8f78be198d3dabcfd130ff12b3c8
lewefysite <felipecaxeiro@gmail.com>
2025-08-25 21:15:56 -0300

    Create register-user.dto.ts

---
+++ backend/src/auth/dto/register-user.dto.ts
@@ -0,0 +1,9 @@
+// Você pode adicionar as propriedades do seu DTO aqui depois.
+// Ex: import { IsString, IsEmail } from 'class-validator';
+export class RegisterUserDto {
+  // @IsEmail()
+  // email: string;
+  
+  // @IsString()
+  // name: string;
+}
