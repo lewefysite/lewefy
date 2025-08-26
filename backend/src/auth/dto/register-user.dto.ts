104bf70a77a1364046d0ae179ed44db63415c35b
lewefysite <felipecaxeiro@gmail.com>
2025-08-25 23:25:28 -0300

    Update register-user.dto.ts

--- backend/src/auth/dto/register-user.dto.ts
+++ backend/src/auth/dto/register-user.dto.ts
@@ -2,12 +2,12 @@ import { IsString, IsEmail, MinLength } from 'class-validator';
 
 export class RegisterUserDto {
   @IsEmail({}, { message: 'Por favor, forneça um endereço de e-mail válido.' })
-  email: string;
+  email!: string;
 
   @IsString()
-  name: string;
+  name!: string;
 
   @IsString()
   @MinLength(6, { message: 'A senha deve ter no mínimo 6 caracteres.' })
-  password: string;
+  password!: string;
 }
