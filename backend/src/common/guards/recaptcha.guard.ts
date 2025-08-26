5be4b57b664dcf96176ad0f1401d6e1135a12c8f
lewefysite <felipecaxeiro@gmail.com>
2025-08-25 23:27:57 -0300

    Update recaptcha.guard.ts

--- backend/src/common/guards/recaptcha.guard.ts
+++ backend/src/common/guards/recaptcha.guard.ts
@@ -1,12 +1,41 @@
-import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
-import { Observable } from 'rxjs';
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { Request } from 'express';
+import axios from 'axios';
 
 @Injectable()
 export class RecaptchaGuard implements CanActivate {
-  canActivate(
-    context: ExecutionContext,
-  ): boolean | Promise<boolean> | Observable<boolean> {
-    // Adicione sua lógica de verificação do reCAPTCHA aqui
-    return true;
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest<Request>();
+    const recaptchaToken = request.body.recaptchaToken;
+
+    if (!recaptchaToken) {
+      throw new UnauthorizedException('Token do reCAPTCHA ausente.');
+    }
+
+    try {
+      const response = await axios.post(
+        `https://www.google.com/recaptcha/api/siteverify?secret=${process.env.RECAPTCHA_SECRET}&response=${recaptchaToken}`
+      );
+
+      const { success } = response.data;
+
+      if (!success) {
+        throw new UnauthorizedException('Falha na verificação do reCAPTCHA.');
+      }
+
+      return true;
+    } catch (error) {
+      if (error instanceof Error) {
+        console.error('Erro na verificação do reCAPTCHA:', error.message);
+      } else {
+        console.error('Erro desconhecido na verificação do reCAPTCHA:', error);
+      }
+      throw new UnauthorizedException('Erro ao verificar o reCAPTCHA.');
+    }
   }
 }
