6de9972b3e1c2805c3b21436dad6f337ffdeaa16
lewefysite <felipecaxeiro@gmail.com>
2025-08-25 23:18:22 -0300

    Update auth.module.ts

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,33 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
-import { UsersModule } from '../users/users.module';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
-import { JwtStrategy } from './strategies/jwt.strategy';
-import { PrismaModule } from 'src/prisma/prisma.module';
-import { RecaptchaGuard } from 'src/common/guards/recaptcha.guard';
-import { HttpModule } from '@nestjs/axios';
-import { EmailModule } from 'src/mail/email.module';
-import { ConfigService } from '@nestjs/config';
+import { JwtStrategy } from './jwt.strategy';
+import { PrismaService } from '../prisma/prisma.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    UsersModule,
     PassportModule,
-    PrismaModule,
-    EmailModule,
-    HttpModule,
     JwtModule.registerAsync({
+      imports: [ConfigModule],
       inject: [ConfigService],
-      useFactory: (config: ConfigService) => ({
-        secret: config.get<string>('JWT_SECRET') || 'default_secret',
-        signOptions: { expiresIn: '60m' },
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: { expiresIn: '1d' },
       }),
     }),
   ],
   controllers: [AuthController],
-  providers: [AuthService, JwtStrategy, RecaptchaGuard],
+  providers: [AuthService, JwtStrategy, PrismaService],
+  exports: [AuthService],
 })
 export class AuthModule {}
-```
