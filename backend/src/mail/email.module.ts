d13c980fa251dd547a3b4fbeb318c90f0c149e0b
lewefysite <felipecaxeiro@gmail.com>
2025-08-25 23:32:36 -0300

    Update email.module.ts

--- backend/src/mail/email.module.ts
+++ backend/src/mail/email.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
-import { HttpModule } from '@nestjs/axios';
 import { EmailService } from './email.service';
 
 @Module({
-  imports: [HttpModule],
   providers: [EmailService],
-  exports: [EmailService], // Exporte o serviço para ser usado em outros módulos
+  exports: [EmailService],
 })
 export class EmailModule {}
