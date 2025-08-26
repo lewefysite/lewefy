e540c10c4687085aa3d0116539c5bd3d72bd42aa
lewefysite <felipecaxeiro@gmail.com>
2025-08-25 22:56:18 -0300

    Update main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,6 +6,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.enableCors();
-  await app.listen(3000);
+
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
 }
 bootstrap();
+
