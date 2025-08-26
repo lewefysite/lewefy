1390af1fece19566a8954bd0c24b03c913aedaf9
lewefysite <felipecaxeiro@gmail.com>
2025-08-25 22:14:34 -0300

    Update users.service.ts

--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -1,13 +1,10 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
-// import * as bcrypt from 'bcrypt'; // Não é mais necessário aqui
 
 @Injectable()
 export class UsersService {
-  constructor(private prisma: PrismaService) {}
+    constructor(private prisma: PrismaService) {}
 
-  async findOne(email: string): Promise<any | undefined> {
-    // Implementação real do Prisma que busca no banco de dados
-    return this.prisma.user.findUnique({ where: { email } });
-  }
+    // Adicione seus métodos de usuário aqui
+    // Ex: findOne, findAll, etc.
 }
