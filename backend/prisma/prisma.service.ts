3d924a3b3cf98b2bbbd17117312d773887248d9c
lewefysite <felipecaxeiro@gmail.com>
2025-08-25 22:36:14 -0300

    Update prisma.service.ts

--- backend/prisma/prisma.service.ts
+++ backend/prisma/prisma.service.ts
@@ -1,9 +1,2 @@
-import { Injectable, OnModuleInit } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
+.gitkeep
 
-@Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit {
-  async onModuleInit() {
-    await this.$connect();
-  }
-}
