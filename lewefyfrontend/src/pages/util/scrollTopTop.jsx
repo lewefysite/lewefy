aa901513847a8747568392737edbc97e62cf0eae
lewefysite <felipecaxeiro@gmail.com>
2025-08-28 15:55:01 -0300

    Update and rename scrolltotop.jsx to scrollTopTop.jsx

--- lewefyfrontend/src/pages/util/scrollTopTop.jsx
+++ lewefyfrontend/src/pages/util/scrollTopTop.jsx
@@ -1,11 +1,11 @@
 import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 
-export default function ScrollToTop() {
+export default function scrollToTop() {
   const { pathname } = useLocation();
 
   useEffect(() => {
-    window.scrollTo(0, 0);
+    window.scrollto(0, 0);
   }, [pathname]);
 
   return null;
