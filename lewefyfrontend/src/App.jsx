0c93156d1d9c8e396877a9db96d49cb786c531dc
lewefysite <felipecaxeiro@gmail.com>
2025-08-28 15:58:17 -0300

    Update App.jsx

--- lewefyfrontend/src/App.jsx
+++ lewefyfrontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
-import scrolltotop from './util/scrolltotop.jsx';
-export default function scrolltotop() {
+import scrollToTop from './util/scrollToTop.jsx';
+export default function scrollToTop() {
   const { pathname } = useLocation();
 
   useEffect(() => {
