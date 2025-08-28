3b324aa0aa3039bb507fb6b86d4b11b00d2449f9
lewefysite <felipecaxeiro@gmail.com>
2025-08-27 22:08:53 -0300

    Add files via upload

---
+++ lewefyfrontend/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import ScrollToTop from './util/scrolltotop.jsx';
+import HomePage from './pages/homepage.jsx';
+import NotFoundPage from './pages/notfoundpage.jsx';
+
+export default function App() {
+  return (
+    <>
+      <ScrollToTop />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </>
+  );
+}
+
