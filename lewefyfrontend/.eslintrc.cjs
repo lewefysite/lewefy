06f8519b703996e4343aded4612e939f267575ec
lewefysite <felipecaxeiro@gmail.com>
2025-08-29 01:00:53 -0300

    Update .eslintrc.cjs

--- lewefyfrontend/.eslintrc.cjs
+++ lewefyfrontend/.eslintrc.cjs
@@ -1,3 +1,4 @@
+// File: lewefyfrontend/.eslintrc.cjs
 module.exports = {
   root: true,
   parserOptions: {
@@ -9,15 +10,26 @@ module.exports = {
     browser: true,
     es2021: true
   },
-  plugins: ['react', 'react-hooks'],
+  plugins: [
+    'react',
+    'react-hooks'
+  ],
   extends: [
     'eslint:recommended',
-    'plugin:react/recommended'
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime'
   ],
   settings: {
-    react: { version: 'detect' }
+    react: {
+      version: 'detect'
+    }
   },
   rules: {
-    // ajuste suas regras aqui
+    // não exige importar React em cada JSX file
+    'react/react-in-jsx-scope': 'off',
+    // desliga a checagem de PropTypes
+    'react/prop-types': 'off',
+    // permite variáveis não usadas em argumentos (prefira prefixar com _ se precisar)
+    'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }]
   }
 };
