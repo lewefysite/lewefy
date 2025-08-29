48c5528effa4060b7c3e454cf7656f6f629a005f
lewefysite <felipecaxeiro@gmail.com>
2025-08-28 22:11:30 -0300

    Update and rename vite.config.ts to vite.config.js remove obsolete vite.config.ts

--- lewefyfrontend/vite.config.js
+++ lewefyfrontend/vite.config.js
@@ -2,20 +2,18 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react()
-  ],
+  plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src')
-    }
+      '@': path.resolve(__dirname, './src'),
+    },
   },
   server: {
-    port: 5173
+    port: 5173,
   },
   build: {
-    outDir: 'dist'
-  }
+    outDir: 'dist',
+  },
 });
-
