4c39164bb602144764030fb1c80ec5eb14eaf41c
lewefysite <felipecaxeiro@gmail.com>
2025-08-28 22:03:47 -0300

    Update vite.config.ts

--- lewefyfrontend/vite.config.ts
+++ lewefyfrontend/vite.config.ts
@@ -1,20 +1,21 @@
-// File: tweeifyfrontend/vite.config.ts
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react()
+  ],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
+      '@': path.resolve(__dirname, './src')
+    }
   },
   server: {
-    port: 5173,
+    port: 5173
   },
   build: {
-    outDir: 'dist',
-  },
+    outDir: 'dist'
+  }
 });
+
