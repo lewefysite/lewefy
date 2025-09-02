a837288ef96383cf87252fa0d6d001b03a39ebb6
lewefysite <felipecaxeiro@gmail.com>
2025-09-02 00:42:56 -0300

    Create Frame.jsx

---
+++ src/pages/Depoimento/sections/Frame.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Frame as CommonFrame } from '../../../components/common/Frame';
+
+export const Frame = () => {
+    // Wrapper para o cabeçalho reutilizável.
+    // Nenhuma lógica específica necessária aqui.
+    return <CommonFrame />;
+};
