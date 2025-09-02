2d067dbbc4b170acd7998ce87462df4c08419c8f
lewefysite <felipecaxeiro@gmail.com>
2025-09-02 00:18:08 -0300

    Create Footer.jsx

---
+++ src/pages/CadastroProfissional/sections/Footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+// ... (importações do componente de rodapé reutilizável)
+
+export const Footer = () => {
+  return (
+    <footer className="inline-flex flex-col items-start absolute top-[2193px] left-0 bg-transparent">
+        {/* ... Conteúdo idêntico ao componente FooterBig já criado ... */}
+    </footer>
+  );
+};
