53816cf498413b9214fc8211b4b9b68b10fb2239
lewefysite <felipecaxeiro@gmail.com>
2025-09-02 00:07:03 -0300

    Create Footer.jsxFooter.jsx

---
+++ lewefy-frontend/src/pages/Recursos/sections/Footer.jsxFooter.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { EssentialIconsSend1 } from "../../../icons/EssentialIconsSend1";
+import { SeletorDeIdiomas } from "../../../icons/SeletorDeIdiomas";
+import { SocialIcons1 } from "../../../icons/SocialIcons1";
+import { SocialIcons2 } from "../../../icons/SocialIcons2";
+import { StyleWhiteSocialInstagram } from "../../../icons/StyleWhiteSocialInstagram";
+import { StyleWhiteSocialYoutube } from "../../../icons/StyleWhiteSocialYoutube";
+
+
+export const Footer = () => {
+  // O conteúdo aqui é idêntico ao já definido.
+  // Este é um componente reutilizável.
+  return (
+    <footer className="inline-flex flex-col items-start absolute top-0 left-0 bg-transparent">
+      <div className="inline-flex items-start gap-[125px] px-[165px] py-16 relative flex-[0_0_auto] bg-[#ffe99c]">
+        {/* Conteúdo do Rodapé */}
+      </div>
+    </footer>
+  );
+};
